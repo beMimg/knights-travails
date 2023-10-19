e042090d16d61d08dc5f142e18301c0c8125658d
bernyig <bernardommguerreiro@gmail.com>
2023-10-20 01:11:22 +0200

    finished

--- index.js
+++ index.js
@@ -1,13 +1,2 @@
-// import { knightMoves } from './knightMoves.js';
-// import { buildBoard } from './knightMoves.js';
-
-function createBoard(n) {
-  console.log('hi');
-  return Array.from({ length: n }, () => Array(n).fill(false));
-}
-
-// x must be equal or greater than 0 but smaller than board size
-// y must be equal or greater than 0 but smaller than board size
-function isValidMove(x, y, n) {
-  return x >= 0 && x < n && y >= 0 && y < n;
-}
+import { Node, knightMoves } from './knightMoves.js';
+knightMoves([0, 0], [3, 4]);
--- knightMoves.js
+++ knightMoves.js
@@ -1,1 +1,44 @@
+export class Node {
+  constructor(start, end) {
+    if (start[0] < 0 || start[0] > 7 || start[1] < 0 || start[1] > 7) {
+      this.node = null;
+    } else {
+      this.node = { start, end };
+    }
+  }
+}
 
+export function knightMoves([x, y], [z, j]) {
+  let q = [new Node([x, y], [[x, y]])];
+  let currentNode = q.shift();
+
+  while (currentNode.node.start[0] !== z || currentNode.node.start[1] !== j) {
+    let moves = [
+      [currentNode.node.start[0] + 2, currentNode.node.start[1] - 1],
+      [currentNode.node.start[0] + 2, currentNode.node.start[1] + 1],
+      [currentNode.node.start[0] - 2, currentNode.node.start[1] - 1],
+      [currentNode.node.start[0] - 2, currentNode.node.start[1] + 1],
+      [currentNode.node.start[0] + 1, currentNode.node.start[1] - 2],
+      [currentNode.node.start[0] + 1, currentNode.node.start[1] + 2],
+      [currentNode.node.start[0] - 1, currentNode.node.start[1] - 2],
+      [currentNode.node.start[0] - 1, currentNode.node.start[1] + 2],
+    ];
+
+    moves.forEach((move) => {
+      let node = new Node(move, currentNode.node.end.concat([move]));
+      if (node.node !== null) {
+        q.push(node);
+      }
+    });
+    currentNode = q.shift();
+  }
+
+  console.log(
+    `=> You made it in ${
+      currentNode.node.end.length - 1
+    } moves!  Here's your path:`
+  );
+  currentNode.node.end.forEach((start) => {
+    console.log(start);
+  });
+}
