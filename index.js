121a09a2e4ae3465309856034c6a28c275c80c58
bernyig <bernardommguerreiro@gmail.com>
2023-10-19 16:55:11 +0200

    buildBoard refator

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 function buildBoard() {
   let arr;
-  for (let positionX = 0; positionX < 8; positionX++) {
-    for (let positionY = 0; positionY < 8; positionY++) {
-      arr = [positionX, positionY];
+  for (let x = 0; x < 8; x++) {
+    for (let y = 0; y < 8; y++) {
+      arr = [x, y];
       console.log(arr);
     }
   }
